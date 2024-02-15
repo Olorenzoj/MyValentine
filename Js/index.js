f61184709c9969d14f01d55c2af75ba83dd73a04
Olorenzoj <olorenzo2004@gmail.com>
2024-02-14 23:49:43 -0600

    last change

--- Js/index.js
+++ Js/index.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const heart = document.querySelector(".heart");
      // Redirigir a la otra página después de cierto tiempo
     setTimeout(function() {
-      window.location.href = "../valentine.html";
+      window.location.href = "../html/valentine.html";
     }, 2300); 
     // Cambiar la escala del corazón a 20 en 1 segundos
     heart.style.transition = "transform 5s ease";
